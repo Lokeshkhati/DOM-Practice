d892f581f2282c2477e3b1c3e8c8304580fad9cf
Lokesh Khati <lokeshkhati005@gmail.com>
2021-12-01 23:41:13 +0530

    Update index.js

--- Practice-10/index.js
+++ Practice-10/index.js
@@ -1,10 +1,10 @@
 const button = document.querySelector('.btn')
 const output = document.querySelector('.output')
 
-let counter = 0
+let counter=0
 output.textContent = counter;
 button.addEventListener("click", () => {
     counter++;
-    output.textContent = counter;
+    output.textContent += counter;
  
   });
